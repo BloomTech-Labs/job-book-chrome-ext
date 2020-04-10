473fdfbbd33f619351a885c6485ad5aa909e826d
spettigrew <sara.pettigrew5@gmail.com>
2020-04-09 19:11:53 -0600

    Added form to popup in file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
-
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Button, Form, Header, Container } from "semantic-ui-react";
 
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
-
-    </div>
-  )
-
+    <Container>
+      <Header as="h3">Save this Job</Header>
+      <Form>
+        <Form.Field>
+          <label>Company Name</label>
+          <input placeholder="Company Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Job Title</label>
+          <input placeholder="Job Title" />
+        </Form.Field>
+        <Form.Field>
+          <label>Job Link</label>
+          <input placeholder="Job Link" />
+        </Form.Field>
+        <Form.Field>
+          <label>Location</label>
+          <input placeholder="Location" />
+        </Form.Field>
+        <Form.Field>
+          <label>Job Rating</label>
+          <input placeholder="Job Rating" />
+        </Form.Field>
+        <Form.Group grouped>
+          <label>"Pin" Job Rating</label>
+          <Form.Field label="1" control="input" type="checkbox" />
+          <Form.Field label="2" control="input" type="checkbox" />
+          <Form.Field label="3" control="input" type="checkbox" />
+        </Form.Group>
+        <Form.TextArea>
+          <label>Description</label>
+          <input placeholder="Description" />
+        </Form.TextArea>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
+  );
 }
 
 export default App;
